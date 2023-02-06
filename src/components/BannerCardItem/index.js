4a2ddc74fb3ef09489683587175419be2f158d56
nagamani <rahul@gamail.com>
2023-02-06 08:54:53 +0000

    solution

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,19 @@
+// Write your code here.
+
+
+import './src/components/BannerCardItem/index.css'
+
+const BannerCardItem=(props)=>
+{
+const {BannerDetails  keyId}=props;
+const{  headerText,description,className}=BannerDetails;
+return(
+    <li  className={className}>
+        <h1>{headerTex}</h1>
+        <p>{descriptio}</p>
+    </li>
+
+
+)
+}
+export default  BannerCardItem
